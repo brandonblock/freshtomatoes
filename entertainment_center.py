0dcfd0fc6fee06233877896829656cb9b358463a
manateemilitia <brandon.block@gmail.com>
2016-02-06 17:23:36 -0500

    _

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,18 +1,19 @@
 import fresh_tomatoes
 import media
 
+# Create six movie objects, passing the strings title, description, poster image, and trailer link.
 movie_a = media.Movie("The Thing",
                       "A group of men in the Antarctic band together to fight an otherworldy evil... but can they trust each other?",
                       "https://upload.wikimedia.org/wikipedia/en/c/c1/ThingPoster.jpg",
                       "https://www.youtube.com/watch?v=VnGBL-qrmvE")
 
 movie_b = media.Movie("Assault on Precinct 13",
-                      "A secratary, police officer, and convicted murderer are forced to cooperate when the local gangs attack a police station.",
+                      "A police officer and a convicted murderer are forced to cooperate when local gangs attack.",
                       "https://upload.wikimedia.org/wikipedia/en/9/9d/Assault_on_precinct_thirteen_movie_poster.jpg",
                       "https://www.youtube.com/watch?v=BaJR2zlnp7o")
 
 movie_c = media.Movie("In the Mouth of Madness",
-                      "An author discovers a mystery and his own madness",
+                      "An author uncovers a mystery and discovers his own madness",
                       "https://upload.wikimedia.org/wikipedia/en/9/92/Mouthmadnessposter.jpg",
                       "https://www.youtube.com/watch?v=mHJl2V1sMxc"
                       )
@@ -32,5 +33,8 @@ movie_f = media.Movie("They Live",
                       "https://upload.wikimedia.org/wikipedia/en/3/3d/1988They_Live_poster300.jpg",
                       "https://www.youtube.com/watch?v=KLRafyWhzG4")
 
+# Put movie objects into a list.
 movies = [movie_a, movie_b, movie_c, movie_d, movie_e, movie_f]
+
+# Past the list of movies to the fresh_tomatoes script.
 fresh_tomatoes.open_movies_page(movies)
---
+++ readme.md
@@ -0,0 +1,3 @@
+Python 2.7 script to create a movie trailer page for all movie objects.
+Edit the movie objects in entertainment_center.py to display your own films.
+The generated fresh_tomatoes.html file can be opened by your browser to display your trailer page.
